939fed7291c5ae8a90aebf69c55ac47cf78d9fee
noricha-vr <noricha.vr@gmail.com>
2023-10-07 15:25:15 +0900

    Change: name Remove: Webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,18 @@
 import * as vscode from 'vscode';
 
-let currentWebviewPanel: vscode.WebviewPanel | undefined = undefined;
-
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.copyAndPaste', async () => {
+    let disposable = vscode.commands.registerCommand('extension.copyAsCodeBlock', async () => {
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
-
-        const file_path = editor?.document?.fileName || "";
-        console.log(file_path);
+        const file_name = editor?.document?.fileName || "";
         const selection = editor.selection;
         const selectedText = editor.document.getText(selection);
-        const text = "```" + file_path + "\n" + selectedText + "\n```";
+        const text = "```" + file_name + "\n" + selectedText + "\n```";
         console.log(text);
-
-        if (currentWebviewPanel) {
-            // Append the new text to the existing webview content
-            currentWebviewPanel.webview.html += `<pre>${text}</pre>`;
-        } else {
-            // Create and show a new webview panel
-            currentWebviewPanel = vscode.window.createWebviewPanel(
-                'selectedTextWebview',
-                'Selected Text',
-                vscode.ViewColumn.One,
-                {}
-            );
-            currentWebviewPanel.webview.html = `<pre>${text}</pre>`;
-
-            // Reset the currentWebviewPanel variable when the panel is disposed
-            currentWebviewPanel.onDidDispose(() => {
-                currentWebviewPanel = undefined;
-            });
-        }
-
+        // copy to clipboard
+        vscode.env.clipboard.writeText(text);
     });
 
     context.subscriptions.push(disposable);
